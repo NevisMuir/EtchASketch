6641f5bb690ea3dcc47c63ed9ad22661babe4ff6
NevisMuir <rmuir310@gmail.com>
2022-05-31 18:10:02 -0400

    Enable draw function on hover border around draw screen

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const container = document.querySelector('#container');
-for(let i=0; i<16;i++){const line = document.createElement('div');
-line.classList.add('line')
-container.appendChild(line)
-for (let i=0; i<16;i++){
-const cell = document.createElement('div')
-cell.classList.add('cell')
-line.appendChild(cell)
+//Grid creation
+for(let i=0; i<16;i++){
+  const line = document.createElement('div');
+  line.classList.add('line')
+  container.appendChild(line)
+  for (let i=0; i<16;i++){
+    const cell = document.createElement('div')
+    cell.classList.add('cell')
+    line.appendChild(cell)
+    cell.addEventListener('mouseenter', ()=>{
+        cell.classList.add('activated')
+    })
 }}
+
