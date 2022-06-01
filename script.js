38853a4d87482dc17b980ee4b588793ec5130d4c
NevisMuir <rmuir310@gmail.com>
2022-06-01 12:05:44 -0400

    Fix container, responsive to pixel change

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const container = document.querySelector('#container');
 //Grid creation
-for(let i=0; i<16;i++){
+for(let i=0; i<64;i++){
   const line = document.createElement('div');
   line.classList.add('line')
   container.appendChild(line)
-  for (let i=0; i<16;i++){
+  for (let i=0; i<64;i++){
     const cell = document.createElement('div')
     cell.classList.add('cell')
     line.appendChild(cell)
