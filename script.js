c193a07438c95a1e8a6d646c55898a85709eb8a7
NevisMuir <rmuir310@gmail.com>
2022-06-01 12:55:47 -0400

    Make working new draw field button

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const container = document.querySelector('#container');
-//Grid creation
-for(let i=0; i<64;i++){
-  const line = document.createElement('div');
-  line.classList.add('line')
-  container.appendChild(line)
-  for (let i=0; i<64;i++){
-    const cell = document.createElement('div')
-    cell.classList.add('cell')
-    line.appendChild(cell)
-    cell.addEventListener('mouseenter', ()=>{
-        cell.classList.add('activated')
+//Grid creation function
+function createGrid(n){
+  for(let i=0; i<n;i++){
+    const line = document.createElement('div');
+    line.classList.add('line')
+    container.appendChild(line)
+    for (let i=0; i<n;i++){
+      const cell = document.createElement('div')
+      cell.classList.add('cell')
+      line.appendChild(cell)
+      cell.addEventListener('mouseenter', ()=>{
+          cell.classList.add('activated')
     })
-}}
+}}}
+
+createGrid(32);
+const newGridButton = document.querySelector('#sizeSelector')
+newGridButton.addEventListener('click', () =>{
+  let length = prompt ("Please enter a length between 4 and 100")
+  if (length>100 || length<4 || isNaN(length)){
+    alert("Error! Please enter a number between 4 and 100")
+  }
+  else {
+    container.textContent="";
+    createGrid(length)
+  }
+})
 
